9cad3d9b017aa67a9726194fa2f1c440984ab925
Taiki Komoda <tkde@hotmail.com>
2025-08-03 12:06:38 +0900

    wip

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,6 @@
-sudo apt update && sudo apt upgrade -y
+sudo apt update && DEBIAN_FRONTEND=noninteractive sudo apt upgrade -y
 wget https://raw.githubusercontent.com/raspberrypi/pico-setup/master/pico_setup.sh
 chmod +x pico_setup.sh
 ./pico_setup.sh
-echo "export PICO_SDK_PATH=/workspaces/GamePadMouse/pico/pico-sdk" > sh.txt
+echo "export PICO_SDK_PATH=/workspaces/GamePadMouse/pico/pico-sdk" > ~/.bashrc
+source ~/.bashrc
